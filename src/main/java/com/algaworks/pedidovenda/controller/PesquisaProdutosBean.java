2c2faf687f6752add785123ee5b6776ed96e934b
Robson Oliveira da Costa <robsondeveloper@gmail.com>
2017-08-01 23:14:04 -0400

    nova anotação @ViewScoped para beans CDI

--- src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -3,7 +3,7 @@ package com.algaworks.pedidovenda.controller;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
